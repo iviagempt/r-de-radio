82ab15697d760006c93febe7be9a94f59275b013
iviagempt <iviagem.pt@gmail.com>
2025-10-22 16:10:53 +0100

    Update page.tsx

--- src/app/privacidade/page.tsx
+++ src/app/privacidade/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Privacidade() {
+  return (
+    <div>
+      <h1>Política de Privacidade</h1>
+      <p>Conteúdo provisório da página de privacidade.</p>
+    </div>
+  );
+}
