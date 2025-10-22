5e492c57e2676def2e15ad7514757cfefda31a52
iviagempt <iviagem.pt@gmail.com>
2025-10-22 19:09:05 +0100

    Update page.tsx

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,6 +1,8 @@
-"use client";
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
+
+"use client";
+
 export default function Premium() {
   return <div>Premium – página em construção.</div>;
 }
