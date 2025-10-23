be5aa9c011080c8c9fafa997287b89a17919ba24
iviagempt <iviagem.pt@gmail.com>
2025-10-23 20:47:01 +0100

    Update AdsenseScript.tsx

--- src/components/AdsenseScript.tsx
+++ src/components/AdsenseScript.tsx
@@ -2,14 +2,16 @@
 import Script from "next/script";
 
 export default function AdsenseScript() {
-  if (!process.env.NEXT_PUBLIC_ADSENSE_CLIENT) return null;
+  const client = process.env.NEXT_PUBLIC_ADSENSE_CLIENT;
+  if (!client) return null;
+
   return (
     <Script
       id="adsense-script"
       async
       strategy="afterInteractive"
       crossOrigin="anonymous"
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT}`}
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
     />
   );
 }
