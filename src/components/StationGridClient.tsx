b11ba0bab71b0d7ea76052eed7d401373a93385a
iviagempt <iviagem.pt@gmail.com>
2025-10-23 23:52:48 +0100

    Update StationGridClient.tsx

--- src/components/StationGridClient.tsx
+++ src/components/StationGridClient.tsx
@@ -5,22 +5,25 @@ declare global {
   interface Window { __playStation?: (s: StationLite) => Promise<void>; }
 }
 
-export default function StationGridClient({ stations }: { stations: StationLite[] }) {
+export default function StationGridClient({ stations }: { stations: any[] }) {
   return (
     <div className="grid-logos">
       {stations.map((s) => (
         <button
           key={s.id}
           className="radio-card"
-          onClick={() => window.__playStation?.(s)}
+          onClick={() =>
+            window.__playStation?.({
+              id: s.id,
+              name: s.name,
+              slug: null,                  // força uso do ID
+              logo_url: s.logo_url ?? null
+            })
+          }
+          type="button"
           title={s.name}
         >
-          {s.logo_url ? (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img src={s.logo_url} alt={s.name} />
-          ) : (
-            <span style={{ color: "#ddd", fontSize: 12, textAlign: "center" }}>{s.name}</span>
-          )}
+          {s.logo_url ? <img src={s.logo_url} alt={s.name} /> : <span>{s.name}</span>}
         </button>
       ))}
     </div>
