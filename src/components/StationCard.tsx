9108e012797712ca46977b6f502e863ed0f1565f
iviagempt <iviagem.pt@gmail.com>
2025-10-24 13:18:58 +0100

    Update StationCard.tsx

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -1,12 +1,39 @@
 "use client";
-import { usePlayer } from "@/context/PlayerContext";
 
-function StationCard({ station, ... }) {
-  const { setStation } = usePlayer();
+import React from "react";
+
+type StationCardProps = {
+  stationId: string;
+  name: string;
+  logo?: string;
+  onClick?: () => void;
+  className?: string;
+};
+
+export default function StationCard({ stationId, name, logo, onClick, className = "" }: StationCardProps) {
   return (
-    <div className="card-link" onClick={() => setStation(station.streamUrl, station.name, station.logo_url, true)}>
-      <img className="logo" src={station.logo_url} alt={station.name} />
-      <div>{station.name}</div>
+    <div
+      key={stationId}
+      className={`card-link ${className}`}
+      role="button"
+      tabIndex={0}
+      onClick={onClick}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          e.preventDefault();
+          onClick?.();
+        }
+      }}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      {logo ? (
+        <img src={logo} alt={name} className="logo" />
+      ) : (
+        <div className="logo-placeholder" aria-hidden>
+          🎧
+        </div>
+      )}
+      <div className="station-name">{name}</div>
     </div>
   );
 }
