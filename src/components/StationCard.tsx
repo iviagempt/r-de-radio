30aa04330dfe69c2b41e29f3b280be3ff0f4e4fe
iviagempt <iviagem.pt@gmail.com>
2025-10-24 05:03:56 +0100

    Update StationCard.tsx

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 export default function StationCard({
-  href,
   name,
   logo,
+  onClick,
 }: {
-  href: string;
   name: string;
   logo?: string;
+  onClick: () => void;
 }) {
   return (
-    <a className="card-link" href={href}>
+    <button className="card-link" onClick={onClick} style={{ flexDirection: "column", textAlign: "center", padding: 20 }}>
       {logo ? (
-        <img className="logo" src={logo} alt={name} />
+        <img className="logo" src={logo} alt={name} style={{ width: 72, height: 72, marginBottom: 10 }} />
       ) : (
-        <span style={{ fontSize: 28 }}>📻</span>
+        <span style={{ fontSize: 56, marginBottom: 10 }}>📻</span>
       )}
-      <span style={{ fontWeight: 600 }}>{name}</span>
-    </a>
+      <span style={{ fontSize: 14, fontWeight: 500, lineHeight: 1.3 }}>{name}</span>
+    </button>
   );
 }
