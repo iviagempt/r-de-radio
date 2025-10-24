d0d6c4330dd063062963e8d7a25781b6d51d9141
iviagempt <iviagem.pt@gmail.com>
2025-10-24 09:18:23 +0100

    Update StationCard.tsx

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -1,32 +1,12 @@
 "use client";
 import FavoriteButton from "./FavoriteButton";
 
-export default function StationCard({
-  stationId,
-  name,
-  logo,
-  onClick,
-}: {
-  stationId: string;
-  name: string;
-  logo?: string;
-  onClick: () => void;
-}) {
+export default function StationCard({ stationId, name, logo, onClick }: { stationId: string; name: string; logo?: string; onClick: () => void; }) {
   return (
     <div style={{ position: "relative" }}>
-      <button
-        className="card-link"
-        onClick={onClick}
-        style={{ flexDirection: "column", textAlign: "center" }}
-      >
-        {logo ? (
-          <img className="logo" src={logo} alt={name} />
-        ) : (
-          <span style={{ fontSize: 64 }}>📻</span>
-        )}
-        <span style={{ fontSize: 14, fontWeight: 500, lineHeight: 1.3, marginTop: 8 }}>
-          {name}
-        </span>
+      <button className="card-link" onClick={onClick} style={{ flexDirection: "column", textAlign: "center" }}>
+        {logo ? <img className="logo" src={logo} alt={name} /> : <span style={{ fontSize: 64 }}>📻</span>}
+        <span style={{ fontSize: 14, fontWeight: 500, lineHeight: 1.3, marginTop: 8 }}>{name}</span>
       </button>
       <div style={{ position: "absolute", top: 8, right: 8, zIndex: 10 }}>
         <FavoriteButton stationId={stationId} size="small" />
