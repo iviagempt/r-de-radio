bea34267ddec6e4823566d20c6be3bea09733ce5
iviagempt <iviagem.pt@gmail.com>
2025-10-24 11:47:41 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ElegantPlayer from "@/components/ElegantPlayer";
-
 export const metadata: Metadata = {
   title: "RDR",
   description: "Ouça rádios do mundo todo, simples e rápido.",
@@ -15,6 +14,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Header />
         {children}
+        <ElegantPlayer
+          streamUrl="URL_DO_STREAM_AQUI"
+          stationName="Nome da Estação"
+          logoUrl="URL_DO_LOGO_AQUI" // opcional
+        />
         <Footer />
       </body>
     </html>
