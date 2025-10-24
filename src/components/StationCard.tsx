f5fff7dfbc243f3a7dc66545f4eea564ae846d4c
iviagempt <iviagem.pt@gmail.com>
2025-10-24 13:05:41 +0100

    Update StationCard.tsx

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -1,16 +1,12 @@
 "use client";
-import FavoriteButton from "./FavoriteButton";
+import { usePlayer } from "@/context/PlayerContext";
 
-export default function StationCard({ stationId, name, logo, onClick }: { stationId: string; name: string; logo?: string; onClick: () => void; }) {
+function StationCard({ station, ... }) {
+  const { setStation } = usePlayer();
   return (
-    <div style={{ position: "relative" }}>
-      <button className="card-link" onClick={onClick} style={{ flexDirection: "column", textAlign: "center" }}>
-        {logo ? <img className="logo" src={logo} alt={name} /> : <span style={{ fontSize: 64 }}>📻</span>}
-        <span style={{ fontSize: 14, fontWeight: 500, lineHeight: 1.3, marginTop: 8 }}>{name}</span>
-      </button>
-      <div style={{ position: "absolute", top: 8, right: 8, zIndex: 10 }}>
-        <FavoriteButton stationId={stationId} size="small" />
-      </div>
+    <div className="card-link" onClick={() => setStation(station.streamUrl, station.name, station.logo_url, true)}>
+      <img className="logo" src={station.logo_url} alt={station.name} />
+      <div>{station.name}</div>
     </div>
   );
 }
