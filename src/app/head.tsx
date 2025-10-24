0cd5ff5c73e54a1a773e22c7ec3c59b6447931e3
iviagempt <iviagem.pt@gmail.com>
2025-10-24 17:30:07 +0100

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  );
+}
