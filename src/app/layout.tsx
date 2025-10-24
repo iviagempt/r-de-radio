abd935e05b66a9309cef4b0637f3fd5a293ca7a4
iviagempt <iviagem.pt@gmail.com>
2025-10-24 18:30:38 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,19 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ElegantPlayer from "@/components/ElegantPlayer";
 import { PlayerProvider } from "@/context/PlayerContext";
-
-export const metadata: Metadata = {
-  title: "RDR",
-  };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt">
       <body>
         <PlayerProvider>
-  <Header />
-  {children}
-  <Footer />
-  <ElegantPlayer />
-</PlayerProvider>
+          <Header />
+          {children}
+          <Footer />
+          <ElegantPlayer />
+        </PlayerProvider>
       </body>
     </html>
   );
